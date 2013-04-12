c02d9826a941e41d8536744abcef1909624f5a01
Peter Jaros <peter.a.jaros@gmail.com>
2013-04-12 14:01:16 -0400

    Store GH app info naively in a free variable.

--- main.rb
+++ main.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
+client_id = nil
+client_secret = nil
+
+class UnprocessableEntity < RuntimeError; end
+
 get '/' do
-  haml :set_up_application
+  if client_id && client_secret
+    "Client ID: #{client_id}\nClient Secret: #{client_secret}"
+  else
+    haml :set_up_application
+  end
+end
+
+post '/set_up_application' do
+  client_id = params.fetch("client_id") { raise UnprocessableEntity }
+  client_secret = params.fetch("client_secret") { raise UnprocessableEntity }
+  redirect '/'
 end
